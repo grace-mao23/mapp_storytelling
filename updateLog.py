0567d58b0533331176c543781f6db0e9171dfced
PrathamRawat <prathamrawat815@gmail.com>
2019-10-18 08:40:03 -0400

    Write script to update devlog with proper format

---
+++ updateLog.py
@@ -0,0 +1,18 @@
+import datetime
+
+print("Please enter your name:")
+print("Format: firstnameL")
+name = input()
+
+print("\n")
+print("Please enter your update:")
+update = input()
+
+timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+
+devlogFile = open("devlog.txt", "a")
+
+devlogFile.write("\n" + name + " -- " + timestamp + "\n")
+devlogFile.write(update)
+
+devlogFile.close()
